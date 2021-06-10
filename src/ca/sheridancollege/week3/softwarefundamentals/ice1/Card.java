174e666cba4491891d9b80dba30a5b26b735dbae
preet1973 <sin11329@sheridancollege.ca>
2021-06-09 20:22:21 -0400

    lucky card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -6,7 +6,7 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
- * A class that models playing card Objects. Cards have 
+ * A class that models playing card Objects. Cards have .
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
